98a0c558a6a85bb854dc29afab94d71022333065
Owen40 <omuthami27@gmail.com>
2023-06-15 16:22:57 +0300

    First Commit

---
+++ app/src/main/java/com/example/rowcolumncompose/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.rowcolumncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rowcolumncompose.ui.theme.RowColumnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RowColumnComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background){
+                    Row(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
+                        CustomItem(weight = 1f)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+//    Surface(modifier = Modifier
+//        .width(50.dp)
+//        .weight(weight),
+//        color = color) {}
+//}
+
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(modifier = Modifier
+        .width(50.dp)
+        .height(50.dp)
+        .weight(weight),
+        color = color) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RowColumnComposeTheme {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+        ) {
+           CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
+            CustomItem(weight = 1f)
+        }
+    }
+}
